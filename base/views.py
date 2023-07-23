deb50960ec63f1873ea605661304a0e651cef938
shoboy09 <clarencepudge4443@gmail.com>
2023-07-23 14:52:54 +0800

    Adding Menu, Reservation and Contact

--- base/views.py
+++ base/views.py
@@ -7,3 +7,12 @@ def home(request):
 
 def about(request):
     return render(request, 'base/about.html')
+
+def reservation(request):
+    return render(request, 'base/reservation.html')
+
+def contact(request):
+    return render(request, 'base/contact.html')
+
+def menu(request):
+    return render(request, 'base/menu.html')
