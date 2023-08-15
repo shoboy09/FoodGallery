cfc88f9b8388147a10a5f566a9da765fc42f0452
shoboy09 <clarencepudge4443@gmail.com>
2023-08-15 20:57:43 +0800

    Adding Date And Time in Dashboard

--- dashboard/static/dashboard/js/dashboard.js
+++ dashboard/static/dashboard/js/dashboard.js
@@ -1,141 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const content = document.querySelector(".content");
-  const bookingContent = document.querySelector(".booking-content");
-  const bookingsDiv = document.querySelector(".bookings-div");
-  let isDragging = false;
-  const dragOffset = { x: 0, y: 0 };
-  const originalPosition = { x: 0, y: 0 };
-
-  // Event listener for the toggle button
-  bookingsDiv.addEventListener("click", () => {
-    if (bookingContent.classList.contains("hidden")) {
-      bookingContent.classList.remove("hidden");
-      resetPosition();
-    } else {
-      bookingContent.classList.add("hidden");
-    }
-  });
-
-  bookingContent.addEventListener("mousedown", (event) => {
-    isDragging = true;
-    dragOffset.x = event.clientX - bookingContent.offsetLeft;
-    dragOffset.y = event.clientY - bookingContent.offsetTop;
-  });
-
-  document.addEventListener("mousemove", (event) => {
-    if (isDragging) {
-      const contentRect = content.getBoundingClientRect();
-      const minX = 0;
-      const minY = 0;
-      const maxX = contentRect.width - bookingContent.offsetWidth;
-      const maxY = contentRect.height - bookingContent.offsetHeight;
-
-      let newX = event.clientX - dragOffset.x;
-      let newY = event.clientY - dragOffset.y;
-
-      // Limit dragging within the container boundaries
-      newX = Math.max(minX, Math.min(newX, maxX));
-      newY = Math.max(minY, Math.min(newY, maxY));
-
-      bookingContent.style.left = newX + "px";
-      bookingContent.style.top = newY + "px";
-    }
-  });
-
-  document.addEventListener("mouseup", () => {
-    isDragging = false;
-    originalPosition.x = bookingContent.offsetLeft;
-    originalPosition.y = bookingContent.offsetTop;
-  });
-
-  // Function to reset the position of the draggable div
-  const resetPosition = () => {
-    bookingContent.style.left = "2%";
-    bookingContent.style.top = "5%";
-  };
-
-  // Function to hide the draggable div when clicking outside
-  const hideOnClickOutside = (event) => {
-    if (
-      !bookingContent.contains(event.target) &&
-      !bookingsDiv.contains(event.target)
-    ) {
-      bookingContent.classList.add("hidden");
-    }
-  };
-
-  // Add click event listener to document
-  document.addEventListener("click", hideOnClickOutside);
-
-  const reserveBtn = document.querySelector(".reserve-btn");
-  const reservationContent = document.querySelector(".reservation-content");
-
-  reserveBtn.addEventListener("click", () => {
-    reservationContent.style.display = "flex";
-  });
-
-  document.addEventListener("click", (event) => {
-    if (
-      !reservationContent.contains(event.target) &&
-      event.target !== reserveBtn
-    ) {
-      reservationContent.style.display = "none";
-    }
-  });
-
-  // const reserveUserIcons = document.querySelectorAll(".reserve-user-icon");
-
-  // reserveUserIcons.forEach((reserveUserIcon) => {
-  //   reserveUserIcon.addEventListener("mouseover", () => {
-  //     reserveUserIcon.classList.remove("fa-regular");
-  //     reserveUserIcon.classList.add("fa-solid");
-  //   });
-
-  //   reserveUserIcon.addEventListener("mouseout", () => {
-  //     reserveUserIcon.classList.remove("fa-solid");
-  //     reserveUserIcon.classList.add("fa-regular");
-  //   });
-  // });
-
-  // Get references to all the available table buttons
-  const reserveButtons = document.querySelectorAll(
-    ".available-table-reserve-btn"
-  );
-  const reservationTable = document.querySelector(".reservation-table");
-
-  // Attach event listeners to each reserve button
-  reserveButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      event.stopPropagation();
-      const tableLabel = event.currentTarget.parentNode.querySelector("label");
-      const tableNumber = tableLabel.textContent.split(" ")[1];
-      const rtbLabel = document.querySelector(".rtb label");
-      rtbLabel.textContent = tableNumber;
-      reservationContent.style.display = "none";
-      reservationTable.style.display = "flex";
-    });
-  });
-
-  document.addEventListener("click", (event) => {
-    const isClickedOutside = !reservationTable.contains(event.target);
-    if (isClickedOutside) {
-      reservationTable.style.display = "none";
-    }
-  });
-
-  const updateDateTime = () => {
-    const date = new Date();
-    const time = date.toLocaleTimeString();
-    const day = date.toLocaleDateString("en-US", { weekday: "short" });
-    const formattedDate = date.toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-    });
-
-    document.querySelector(".rta-time").textContent = time;
-    document.querySelector(".rta-date").textContent = `${day} ${formattedDate}`;
-  };
-
-  setInterval(updateDateTime, 1000);
+document.addEventListener("DOMContentLoaded", function () {
+  function updateTime() {
+    const currentTimeElement = document.querySelector(".time");
+    const currentDateElement = document.querySelector(".date");
+    const timeNow = new Date();
+    const hours = (timeNow.getHours() % 12 || 12).toString().padStart(2, "0");
+    const minutes = timeNow.getMinutes().toString().padStart(2, "0");
+    const seconds = timeNow.getSeconds().toString().padStart(2, "0");
+    const ampm = timeNow.getHours() >= 12 ? "PM" : "AM";
+    const timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
+    currentTimeElement.textContent = timeString;
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    const dateNow = new Date();
+    const dateString = dateNow.toLocaleDateString("en-US", options);
+    currentDateElement.textContent = dateString;
+  }
+  setInterval(updateTime, 1000);
+  updateTime();
 });
